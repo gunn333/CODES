f557c1b875b36fdb3f2805784db181cac6f48b25
Gunn <gunn333.be22@chitkara.edu.in>
2023-09-18 21:00:11 +0530

    Added

---
+++ C++/basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+        cout << "Hello World\n" <<endl;
+        cout << endl;
+        cout << '\n';
+
+    endl , \n is for new line
+    is for comment */
+    cout << "Enter a number -> "; // print
+    int a;                        // declare a variable
+    cin >> a;                     // input >> jisko lena hai
+    cout << "You Entered " << a << endl;
+}

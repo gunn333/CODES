96e27fcbfea1796073f6f4828e6c0f4e1044430a
Gunn <gunn333.be22@chitkara.edu.in>
2023-09-20 14:39:47 +0530

    ADDed

--- C++/test.cpp
+++ C++/test.cpp
@@ -312,3 +312,45 @@ int main(){
     return 0;
 }
 */
+
+#include <iostream>
+#include <algorithm>
+
+// Function to perform binary search on a sorted array
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Element found, return its index
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search the right half
+        } else {
+            right = mid - 1; // Search the left half
+        }
+    }
+
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+
+    // Sort the array before performing binary search
+    std::sort(arr, arr + size);
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        std::cout << "Element " << target << " found at index " << result << std::endl;
+    } else {
+        std::cout << "Element " << target << " not found in the array." << std::endl;
+    }
+
+    return 0;
+}

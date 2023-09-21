56a0a496697dae3214d80c9a3ba5a57a015392ed
Gunn <gunn333.be22@chitkara.edu.in>
2023-09-21 12:37:31 +0530

    Added

--- C++/uniquearr.cpp
+++ C++/uniquearr.cpp
@@ -1,20 +1,25 @@
 //! Find Unique element in array
-
+/*
 #include <iostream>
 using namespace std;
 int Uniqueele(int arr[],int n){
-    for(int i = 0; i < n; i++){
-        int j;
-        for(j = 0; j < n; j++){  //j loop is used to check if there is any duplicate element in array
-            if(i != j && arr[i] == arr[j]){
+    cout << "Unique elements in the array: ";
+
+    for (int i = 0; i < n; ++i) {
+        bool isUnique = true;
+
+        // Check if the current element is repeated later in the array
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[i] == arr[j]) {
+                isUnique = false;
                 break;
             }
         }
-        if(j == n){
+        // If the element is unique, print it
+        if (isUnique) {
             cout << arr[i] << " ";
         }
     }
-    
     return 0;
 }
 
@@ -29,5 +34,36 @@ int main()
     }
     Uniqueele(arr,n);
 
+    return 0;
+}
+*/
+// Alternative method
+#include <iostream>
+using namespace std;
+
+int uniqueele(int arr[], int n)
+{
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans ^= arr[i];
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << uniqueele(arr, n);
     return 0;
 }

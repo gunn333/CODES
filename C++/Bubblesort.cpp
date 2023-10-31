6ab47d4836edcf106b0bd4150a91c0442403c91f
Gunn <gunn333.be22@chitkara.edu.in>
2023-10-31 12:10:44 +0530

    ADDED

---
+++ C++/Bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements of array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "\n";
+    cout <<"Unsorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    bubbleSort(arr, n);
+    cout << "\n" << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}

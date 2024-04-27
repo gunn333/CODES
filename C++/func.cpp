50271c71294f9d59895893ca0b155799b5f03def
gunn333 <122155330+gunn333@users.noreply.github.com>
2024-04-27 20:00:36 +0530

    💜

--- C++/func.cpp
+++ C++/func.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void HappyBirthday(string name, int age)
-{                                                 // void function_name() {function body}
-    cout << "Happy Birthday to " << name << "\n"; // can change name to any string like bdayboy it will work the same as name
+void HappyBirthday(){    //void function_name() {function body}
+    cout << "Happy Birthday to you!\n";
     cout << "Happy Birthday to you!\n";
     cout << "Happy Birthday dear user!\n";
     cout << "Happy Birthday to you!\n";
-    cout << "You are " << age << " years old!\n";
 }
-int main()
-{
+int main(){
     /*
     function = a block of code that is executed whenever it is called upon
     function prototype = a function declaration that tells the compiler about a function's name, return type, and parameters
@@ -33,16 +30,8 @@ int main()
     local scope = the part of a program in which a local variable may be accessed
     global scope = the part of a program in which a global variable may be accessed
     */
-    string name = "Bro";
-    int age = 18;
-    HappyBirthday(name, age); // function call
+    HappyBirthday(); //function call
     // can call the function as many times as you want
-
-    /*
-    Need to define void happybirthday();
-    if we are using after main function
-    void function returns nothing
-    can pass Function in if else statement
-    */
+    
     return 0;
 }
